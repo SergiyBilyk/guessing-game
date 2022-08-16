0043ad3f183c13412f1460510884bac0680ea6db
SergiyBilyk <102257452+SergiyBilyk@users.noreply.github.com>
2022-08-17 00:44:51 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
-
+    numb;
+    arr = [];
     setRange(min, max) {
-
+        for (let i = min + 1; i < max; i++) {
+            this.arr.push(i);
+        }
+        this.numb = Math.ceil((this.arr.length - 1) / 2);
     }
-
     guess() {
-
+        return this.arr[this.numb];
     }
-
     lower() {
-
+        this.arr = this.arr.slice(0, this.numb);
+        this.numb = Math.floor(this.arr.length / 2);
     }
-
     greater() {
-
+        this.arr = this.array.slice(this.numb + 1);
+        this.numb = Math.floor(this.arr.length / 2);
     }
 }
 
